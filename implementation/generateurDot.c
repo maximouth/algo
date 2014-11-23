4a73f4bdab431a49b1d60b39781a752399678d80
Moi-meme <maximouth@localhost.localdomain>
2014-11-23 14:54:53 +0100

    sujet

--- implementation/generateurDot.c
+++ implementation/generateurDot.c
@@ -21,7 +21,7 @@ void generateur(FILE *f, Graph *g) {
 
   fprintf(f,"graph g { \n\trankdir = LR\n\tbgcolor = grey50\nsize = \"1500\"\n");
   //xfprintf(f,"graph [bb=\"0,0,10000,10000\"]");
-  fprintf(f,"\tnode [\n\tfrontsize = \"1\"\n\tshape = box\n\tstyle = \"rounded, filled\"\n\t]\n\n");
+  fprintf(f,"\tnode [\n\tfrontsize = \"0.001\"\n\tstyle = \"rounded, filled\"\n\t]\n\n");
   
   for (i = 0; i< g->size ; i++) {
     fprintf(f,"\tmyNode%d [\n",i);
@@ -30,6 +30,7 @@ void generateur(FILE *f, Graph *g) {
     fprintf(f,"\tpos = \"%d,%d\"\n",g->Point[i]->x,g->Point[i]->y);
     fprintf(f,"\t]\n\n");
     }
+  
   
   for (i = 0; i< g->size ; i++) {
     while (g->Point[i]->voisin) {
@@ -41,7 +42,7 @@ void generateur(FILE *f, Graph *g) {
     g->Point[i]->voisin = g->Point[i]->voisin->suiv;
       
     }
-  }
+    } 
   
 
 fprintf(f,"}");
