825ce198e6dbe0cee1dc43d959cc54b1ea06dc3d
Moi meme <philippe.ayrault@etersafe.com>
2014-11-29 14:50:44 +0100

    Ajout du fichier graphe des distances

---
+++ implementation/graphe_distance.c
@@ -0,0 +1,12 @@
+#include "reseau.h"
+#include "tas_dijkstra.h"
+#include "dijkstra.h"
+
+
+
+
+Reseau* grapheDistance(Reseau *res)
+{
+  return NULL;
+}
+
---
+++ implementation/graphe_distance.h
@@ -0,0 +1,9 @@
+# ifndef __GRAPHE_DIST_H__
+# define __GRAPHE_DIST_H__
+
+#include "reseau.h"
+
+/* Creation d'un grahe de distance */
+CellNoeud* grapheDistance(Reseau *res);
+
+#endif
